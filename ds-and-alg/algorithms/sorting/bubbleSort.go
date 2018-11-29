a0323f9ac63bfba3062a5c0b7c3ca8d9a0276fc1
kgtom <lwz0538@gmail.com>
2018-11-29 23:29:11 +0800

    Create bubbleSort.go

---
+++ ds-and-alg/algorithms/sorting/bubbleSort.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//整形切片
+	arr := []int{3, 2, 1, 6}
+	bubbleSort2(arr)
+	fmt.Println("arr:", arr)
+
+}
+
+//相邻数比较，大的放后面，小的放前面。time O(n^2) space O(n)
+func bubbleSort(arr []int) {
+
+	//外层循环 控制循环轮次，内层循环 控制每轮的比较次数。
+	for i := 0; i < len(arr); i++ {
+		//每一轮都要进行两两比较
+		for j := 0; j < len(arr)-1-i; j++ {
+
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				fmt.Println(arr)
+			}
+		}
+	}
+}
+
+//注意：如果在某次遍历中没有发生交换，那么就不必进行下次遍历，因为序列是有序的。
+//此方法减少遍历次数
+func bubbleSort2(arr []int) {
+	var isSort = true
+	for i := 0; i < len(arr) && isSort; i++ {
+		isSort = false
+		for j := 0; j < len(arr)-1-i; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+				isSort = true
+			}
+		}
+
+	}
+}
