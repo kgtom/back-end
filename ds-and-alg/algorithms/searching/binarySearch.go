d858e58f2ea1d23a77cd12290e5edf7420613852
kgtom <lwz0538@gmail.com>
2018-12-01 22:06:50 +0800

    Update binarySearch.go

--- ds-and-alg/algorithms/searching/binarySearch.go
+++ ds-and-alg/algorithms/searching/binarySearch.go
@@ -7,12 +7,14 @@ import (
 func main() {
 
 	arr := []int{1, 2, 3, 4, 5, 6, 7}
-	ret := binarySearch(arr, 0, len(arr), 5)
+	//ret := binarySearch2(arr, 0, len(arr), 5)
+	ret := binarySearch(arr, len(arr), 5)
 	fmt.Println("ret:", ret)
 
 }
 
-//二分查找--迭代版：采取分治策略。在顺序列表中，折半查找，一边大、一边小，不断递归查找。
+//二分查找--迭代版：采取分治策略。在顺序列表中，折半查找，一边大、一边小，不断迭代查找。
+//循环相比递归少了很多内存分配和压栈的操作,开销会少很多，最好做法通过循环迭代实现。
 // time:O(log^n)
 func binarySearch(arr []int, n int, searchVal int) int {
 	//定义查找区间为[l,r]
@@ -22,7 +24,7 @@ func binarySearch(arr []int, n int, searchVal int) int {
 
 	for l <= r {
 		mid := l + (r-l)/2
-		fmt.Println("mid:", mid)
+
 		if arr[mid] == searchVal {
 			return mid
 		}
@@ -37,7 +39,7 @@ func binarySearch(arr []int, n int, searchVal int) int {
 	return -1
 }
 
-//二分查找--递归版 time:O(log^n)
+//二分查找--递归版
 func binarySearch2(arr []int, l, r int, searchVal int) int {
 
 	mid := l + (r-l)/2
